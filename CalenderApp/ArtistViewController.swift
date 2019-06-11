08ffadd84adace3e01c0f49dea45fd66720fcd91
Yuuki.Maruyama <sorachikuroneko@gmail.com>
2019-06-11 13:56:47 +0900

    必要な画面の数だけviewcontrollerを配置

---
+++ CalenderApp/ArtistViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ArtistViewController.swift
+//  CalenderApp
+//
+//  Created by まるやまゆうき on 2019/06/10.
+//  Copyright © 2019 yuuki.maruyama. All rights reserved.
+//
+
+import UIKit
+
+class ArtistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+
+}
---
+++ CalenderApp/LiveViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LiveViewController.swift
+//  CalenderApp
+//
+//  Created by まるやまゆうき on 2019/06/10.
+//  Copyright © 2019 yuuki.maruyama. All rights reserved.
+//
+
+import UIKit
+
+class LiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+
+}
