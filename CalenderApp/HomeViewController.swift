c5e7911f3d14d9f7a70290ef41f0188ee45ee689
Yuuki.Maruyama <sorachikuroneko@gmail.com>
2019-06-12 16:55:05 +0900

    20190612 grade

--- CalenderApp/HomeViewController.swift
+++ CalenderApp/HomeViewController.swift
@@ -7,24 +7,26 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var debugLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let name = Auth.auth().currentUser?.displayName
+        debugLabel.text = "DEBUG:" + String(name!)
+        
+//        let grade = Auth.auth().currentUser?.grade
+//        debugLabel.text = "DEBUG:" + String(grade!)
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
-    */
 
 }
